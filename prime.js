5cd2d3454aa86e85bf193d73995184353407e079
shiprasaini99 <shiprasaini2012@gmail.com>
2022-09-13 23:52:46 +0530

    COmmited updated code

--- prime.js
+++ prime.js
@@ -1,2 +1,40 @@
-var 
-var
+var x=13;
+var ifPrime=true;
+if(x===1){
+console.log("1 is not a prime");
+}
+else if(x>1){
+
+for(var i=2; i<x; i++){
+if(x%i==0){
+ifPrime=false;
+break;
+
+}
+}
+if(ifPrime){
+console.log("Yes");
+}
+else{
+console.log("No");
+}
+
+
+// palindrome---------------------------
+
+var str="naman";
+var x=str.length;
+var count=0;
+
+for(var i=0; i<x; i++){
+if(str[i]!=str[x-i-1]){
+count++;
+}
+}
+if(count==0){
+console.log("Yes");
+}
+else{
+console.log("No");
+}
+
